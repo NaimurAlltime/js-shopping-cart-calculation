3c693f12a357f9d2183cbe9f09be532a514c00cd
Naimur Rahman <naimur2231@gmail.com>
2023-02-14 22:24:36 +0600

    case price cacultaion complete

---
+++ js/case.js
@@ -0,0 +1,29 @@
+function updateCaseNumber(isIncrease){
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
+    let newCaseNumber;
+    if(isIncrease === true){
+        newCaseNumber = previousCaseNumber + 1;
+    }else{
+        newCaseNumber = previousCaseNumber - 1;  
+    }
+    caseNumberField.value = newCaseNumber;
+    return newCaseNumber;
+}
+
+function updateCaseTotalPrice(newCaseNumber){
+    const totalPrice = newCaseNumber * 59;
+    const casePriceElement = document.getElementById('case-total-price');
+    casePriceElement.innerText = totalPrice;
+}
+
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+  const newCaseNumberPlus = updateCaseNumber(true);
+  updateCaseTotalPrice(newCaseNumberPlus);
+})
+
+document.getElementById('btn-case-minus').addEventListener('click', function(){
+    const newCaseNumberMinus = updateCaseNumber(false);
+    updateCaseTotalPrice(newCaseNumberMinus);
+})
